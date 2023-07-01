46eaab7e0946903791bc14d2bffc27acd247c45e
Fadhil Yori Hibatullah <me@fadhilyori.my.id>
2023-07-02 02:16:13 +0700

    add subping package and implement it in main.go

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ subping is a powerful and user-friendly command-line tool that allows you to per
 addresses within a specified subnet range. With subping, you can effortlessly discover and monitor the availability of
 devices within a network by systematically pinging each IP address within the defined subnet.
 
+## Dependencies
+
+Subping depends on the following third-party libraries:
+
+- **go-ping**: https://github.com/go-ping/ping
+
 ## Usage
 
 To use subping, follow these steps:
@@ -16,8 +22,95 @@ To use subping, follow these steps:
 3. Run the subping command with the specified subnet range:
 
    ```shell
-   subping <subnet>
+   subping [OPTIONS] <network subnet>
    ```
+
+   **Options:**
+
+   `-c <count>`: Specifies the number of ping attempts for each IP address. The default value is 3.
+
+   `-n <numJobs>`: Specifies the number of maximum concurrent jobs spawned to perform ping operations. The default value
+   is equal to the number of CPUs available on the system.
+
+   `-t <timeout>`: Specifies the maximum ping timeout duration. The default value is "300ms". The timeout can be
+   expressed in various units
+
+## Import as Go Package
+
+To use the Subping library, follow these steps:
+
+1. Import the Subping package:
+
+```go
+import (
+    "github.com/your-username/subping"
+    "github.com/your-username/subping/pkg/network"
+)
+```
+
+2. Create an instance of Subping by calling `NewSubping` with the desired options:
+
+```go
+subnetString := "172.17.0.0/24"
+targets, err := network.GenerateIPListFromCIDRString(subnetString)
+if err != nil {
+    log.Fatal(err.Error())
+}
+
+opts := &subping.Options{
+    Targets: targets,
+    Count:   3,
+    Timeout: 300 * time.Millisecond,
+    NumJobs: 8,
+}
+
+sp, err := subping.NewSubping(opts)
+if err != nil {
+    log.Fatal(err)
+}
+
+```
+
+Note: Ensure that you have imported the necessary packages, such as `"time"` and `"log"`.
+
+3. Run the Subping process by calling the `Run` method:
+
+```go
+sp.Run()
+```
+
+This will initiate the ICMP ping operations on the specified IP addresses.
+
+4. Retrieve the results using the `GetResults` method:
+
+```go
+results := sp.GetResults()
+```
+
+The `results` variable will contain a map where the keys are the IP addresses, and the values are `*ping.Statistics`
+representing the ping statistics for each IP address.
+
+5. Optionally, you can use the `GetOnlineHosts` method to filter the results and obtain only the IP addresses that
+   responded
+   to the ping:
+
+```go
+onlineHosts := sp.GetOnlineHosts()
+```
+
+The `onlineHosts` variable will contain a map of the online IP addresses and their corresponding ping statistics.
+
+6. You can also call the `RunPing` function directly to perform a ping operation on a single IP address:
+
+```go
+ipAddress := net.ParseIP("192.168.1.1")
+count := 3
+timeout := 300 * time.Millisecond
+
+stats := subping.RunPing(ipAddress, count, timeout)
+```
+
+The `stats` variable will contain the ping statistics for the specified IP address.
 
 ## Examples
 
@@ -26,7 +119,7 @@ Here are a few examples of how to use subping:
 Ping all IP addresses in the subnet range 172.17.0.0/24:
 
 ```shell
-subping 172.17.0.0/24
+subping -t 300ms -c 3 -n 100 172.17.0.0/24
 ```
 
 ![](docs/images/usage-example.png?raw=true)
--- cmd/subping/main.go
+++ cmd/subping/main.go
@@ -1,100 +1,97 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"math"
-	"net"
 	"os"
 	"runtime"
-	"sync"
+	"sort"
 	"time"
 
+	"github.com/fadhilyori/subping"
 	"github.com/fadhilyori/subping/pkg/network"
-	"github.com/go-ping/ping"
-	_ "go.uber.org/automaxprocs"
 )
 
-var (
-	wg sync.WaitGroup
-)
-
-func doPing(ipAddress net.IP, count int, timeout time.Duration) *ping.Statistics {
-	pinger, err := ping.NewPinger(ipAddress.String())
-	if err != nil {
-		log.Printf("Failed to create pinger for IP Address: %s\n", ipAddress)
-		return nil
-	}
-
-	pinger.Count = count
-	pinger.Timeout = timeout
-	err = pinger.Run()
-	if err != nil {
-		return nil
-	}
-
-	return pinger.Statistics()
-}
-
-func partitionSlice(arr []net.IP, numPartitions int) [][]net.IP {
-	arrSize := len(arr)
-	chunkSize := int(math.Ceil(float64(arrSize) / float64(numPartitions)))
-
-	var result [][]net.IP
-
-	for i := 0; i < arrSize; i += chunkSize {
-		end := i + chunkSize
-		if end > arrSize {
-			end = arrSize
-		}
-
-		result = append(result, arr[i:end])
-	}
-
-	return result
+type flagConfig struct {
+	count   int
+	timeout time.Duration
+	numJobs int
 }
 
 func main() {
-	subnetString := os.Args[1]
+	subnetString := os.Args[len(os.Args)-1]
+
+	config := flagConfig{}
+	var timeoutStr string
+
+	flag.IntVar(&config.count, "c", 3, "Specifies the number of ping attempts for each IP address.")
+	flag.IntVar(&config.numJobs, "n", runtime.NumCPU(), "Specifies the number of maximum concurrent jobs spawned to perform ping operations.\nThe default value is equal to the number of CPUs available on the system.")
+	flag.StringVar(&timeoutStr, "t", "300ms", "Specifies the maximum ping timeout duration. The default value is \"300ms\".")
+	flag.Usage = func() {
+		_, err := fmt.Fprintln(flag.CommandLine.Output(), "Usage:\n\nsubping [OPTIONS] <network subnet>\n\nOptions:")
+		if err != nil {
+			return
+		}
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	startTime := time.Now()
+
+	t, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	config.timeout = t
+
 	ips, err := network.GenerateIPListFromCIDRString(subnetString)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	workersCount := runtime.NumCPU()
-	ipsSplit := partitionSlice(ips, workersCount)
 
-	fmt.Printf("Network\t\t\t: %s\n", subnetString)
-	fmt.Printf("IP addresses ranges\t: %v - %v\n", ips[0], ips[len(ips)-1])
-	fmt.Printf("Total hosts\t\t: %d\n", len(ips))
-	fmt.Printf("Partition\t\t: %d\n", len(ipsSplit))
-	fmt.Println("-----------------------------------")
-	fmt.Println("IP Address      | Avg Latency     |")
-	fmt.Println("-----------------------------------")
-
-	for _, ipJob := range ipsSplit {
-		ipQueue := ipJob
-		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-
-			for _, ip := range ipQueue {
-
-				p := doPing(ip, 3, 3*time.Millisecond)
-
-				if p == nil {
-					continue
-				}
-
-				if p.PacketsSent == 0 || p.PacketsRecv == 0 {
-					continue
-				}
-
-				fmt.Printf("%-16s| %-16s|\n", ip, p.AvgRtt.String())
-			}
-		}()
+	s, err := subping.NewSubping(&subping.Options{
+		Targets: ips,
+		Count:   config.count,
+		Timeout: config.timeout,
+		NumJobs: config.numJobs,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	wg.Wait()
-	fmt.Println("-----------------------------------")
+	fmt.Printf("Network\t\t: %s\n", subnetString)
+	fmt.Printf("IP Ranges\t: %v - %v\n", ips[0], ips[len(ips)-1])
+	fmt.Printf("Total hosts\t: %d\n", len(ips))
+	fmt.Println("---------------------------------------")
+	fmt.Println("| IP Address       | Avg Latency      |")
+	fmt.Println("---------------------------------------")
+	fmt.Printf("Pinging...")
+
+	s.Run()
+
+	results := s.GetOnlineHosts()
+
+	// Extract keys into a slice
+	keys := make([]string, 0, len(results))
+	for key := range results {
+		keys = append(keys, key)
+	}
+
+	// Sort the keys
+	sort.Strings(keys)
+
+	fmt.Print("\r")
+
+	for _, ip := range keys {
+		stats := results[ip]
+
+		fmt.Printf("| %-16s | %-16s |\n", ip, stats.AvgRtt.String())
+	}
+
+	fmt.Println("---------------------------------------")
+
+	elapsed := time.Since(startTime)
+	fmt.Printf("Execution time: %s\n", elapsed.String())
 }
